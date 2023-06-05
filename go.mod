717076d1fa14615b6ad3a02729ebb32e42fec830
Prasang Misra <prasangmisra@gmail.com>
2023-06-05 18:23:13 -0400

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/prasangmisra/golatex
+
+go 1.19
---
+++ pdf_generator.go
@@ -0,0 +1,35 @@
+package golatex
+
+import (
+	"io/ioutil"
+	"os"
+	"os/exec"
+)
+
+// GeneratePDF generates a PDF document from LaTeX source
+func GeneratePDF(latexSource string) error {
+	// Create a temporary file with .tex extension
+	tmpfile, err := ioutil.TempFile("", "*.tex")
+	if err != nil {
+		return err
+	}
+	defer os.Remove(tmpfile.Name())
+
+	// Write the LaTeX source to the temporary file
+	if _, err := tmpfile.WriteString(latexSource); err != nil {
+		return err
+	}
+
+	// Close the temporary file
+	if err := tmpfile.Close(); err != nil {
+		return err
+	}
+
+	// Execute LaTeX command to generate PDF
+	cmd := exec.Command("pdflatex", "-interaction=batchmode", tmpfile.Name())
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	return nil
+}
