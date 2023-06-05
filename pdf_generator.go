b9ffc18c9a7cf2993d90f83d058367d158495095
Prasang Misra <prasangmisra@gmail.com>
2023-06-05 18:43:16 -0400

    Added functionality to check if latex exists and docker is present

--- pdf_generator.go
+++ pdf_generator.go
@@ -1,6 +1,7 @@
 package go_latex
 
 import (
+	"errors"
 	"io/ioutil"
 	"os"
 	"os/exec"
@@ -8,6 +9,15 @@ import (
 
 // GeneratePDF generates a PDF document from LaTeX source
 func GeneratePDF(latexSource string) error {
+	// Check if LaTeX is installed
+	if !isLaTeXInstalled() {
+		// If not installed, try starting the Docker container
+		if err := startLaTeXDockerContainer(); err != nil {
+			return err
+		}
+	}
+
+	// ... Rest of the function code ...
 	// Create a temporary file with .tex extension
 	tmpfile, err := ioutil.TempFile("", "*.tex")
 	if err != nil {
@@ -32,4 +42,40 @@ func GeneratePDF(latexSource string) error {
 	}
 
 	return nil
+}
+
+// isLaTeXInstalled checks if LaTeX is installed on the system
+func isLaTeXInstalled() bool {
+	cmd := exec.Command("pdflatex", "--version")
+	err := cmd.Run()
+	return err == nil
+}
+
+// startLaTeXDockerContainer starts a Docker container with a LaTeX image
+func startLaTeXDockerContainer() error {
+	// Check if Docker is installed
+	if !isDockerInstalled() {
+		return errors.New("docker is not installed")
+	}
+
+	// Pull the LaTeX Docker image
+	cmd := exec.Command("docker", "pull", "texlive/texlive")
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	// Run the LaTeX Docker container in the background
+	cmd = exec.Command("docker", "run", "-d", "texlive/texlive")
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// isDockerInstalled checks if Docker is installed on the system
+func isDockerInstalled() bool {
+	cmd := exec.Command("docker", "--version")
+	err := cmd.Run()
+	return err == nil
 }
